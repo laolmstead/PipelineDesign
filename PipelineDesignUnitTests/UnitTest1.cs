92f34cfbcd96f6b70bf0a562bfb79e009fdb104b
laolmstead <laolmstead19@gmail.com>
2022-02-28 22:55:29 -0500

    Create test project

---
+++ PipelineDesignUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PipelineDesignUnitTests
+{
+    [TestClass]
+    public class CrossSectionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
